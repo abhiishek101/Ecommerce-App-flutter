811ad2023f3b028910713a064ed0d55ec9ebe7f2
abhiishek101 <159621162+abhiishek101@users.noreply.github.com>
2025-12-06 23:58:09 +0530

    Update db_service.dart
    db services

--- lib/controllers/db_service.dart
+++ lib/controllers/db_service.dart
@@ -1,1 +1,25 @@
-class
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class DbServices {
+Stream<QuerySnapshot> readCategories(){
+  return FirebaseFirestore.instance
+  .collection("shop_categories")
+  .snapshots();
+}
+//create new category
+Future createCategories({required Map<String,dynamic>data}) async{
+  await FirebaseFirestore.instance.collection("shop_categories").add(data);
+}
+
+//update category
+Future updateCategories({required String docId, required Map<String,dynamic>data}) async{
+  await FirebaseFirestore.instance.collection("shop_categories").doc(docId).update(data);
+}
+
+//update category
+Future deleteCategories({required String docId,}) async{
+  await FirebaseFirestore.instance.collection("shop_categories").doc(docId).delete();
+}
+
+}
